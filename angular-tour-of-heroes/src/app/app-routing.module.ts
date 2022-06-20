2582ebecda0548a5e5e09242b3139e789e0a4d0b
AlexandraHessen <xl_energy@mail.ru>
2022-06-20 16:59:18 +0300

    Add Navigation

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//RouterModule, Routes - импортируем чтобы приложение мого имеьт функцию маршрутизации
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }, //Символ двоеточия ( :) pathуказывает на то, что :idэто заполнитель для конкретного героя id.
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+//exports: [ RouterModule ] -  экспортируем чтобы был доступен во всем приложении
+//Метод вызывается, forRoot()потому что вы настраиваете маршрутизатор на корневом уровне приложения. Метод forRoot()предоставляет поставщиков услуг и директивы, необходимые для маршрутизации, и выполняет начальную навигацию на основе текущего URL-адреса браузера.
+
+export class AppRoutingModule {}
